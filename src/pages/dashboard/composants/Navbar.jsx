6c22d7267c9baa3a8f0c72437aa6e8adb9e5c5d4
Steve Vandenbossche <90630250+ecomdesignbe@users.noreply.github.com>
2024-08-17 17:43:55 +0200

    Update Navbar.jsx

--- src/pages/dashboard/composants/Navbar.jsx
+++ src/pages/dashboard/composants/Navbar.jsx
@@ -29,7 +29,7 @@ export default function Navbar() {
 
     const deconnexion = () => {
         localStorage.removeItem('utilisateur')
-        window.location.replace('/connexion')
+        window.location.replace('/')
     }
 
     return (
